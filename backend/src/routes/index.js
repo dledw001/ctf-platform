173da0145e56aef405e2e05992203fd4efb88016
Dan Ledwith <dledw001@odu.edu>
2025-12-07 16:46:01 -0500

    removed test route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -7,9 +7,4 @@ const challengesRoutes = require('./challenges');
 router.use('/auth', authRoutes);
 router.use('/challenges', challengesRoutes);
 
-// test route
-router.get('/test', (req, res) => {
-    res.json({message: 'CTF API up and running...'});
-});
-
 module.exports = router;
